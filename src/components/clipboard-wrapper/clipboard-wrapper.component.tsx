74ec47d2a9ca852bcc5e5e1fa839169483789348
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-01-07 16:01:22 +0100

    fix copy method for Safari

--- src/components/clipboard-wrapper/clipboard-wrapper.component.tsx
+++ src/components/clipboard-wrapper/clipboard-wrapper.component.tsx
@@ -46,30 +46,50 @@ export class ClipboardWrapper {
             this.anyNavigator.clipboard.writeText(newText)
                 .then(() => this.updateInternalClipboardText(newText));
         } else {
+            // Textarea solution with fixes for iOS
+            // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
             // prepare helper element for writing string into clipboard
-            const input = document.createElement('input');
-            document.body.appendChild(input);
-            input.value = newText;
-            // Google Solution
-            // input.focus();
-            // input.select();
+            const textarea = document.createElement('textarea');
 
-            // whatwebcando.today solution
-            // clear current selection
-            const selection = window.getSelection();
-            selection.removeAllRanges();
+            // Move it off screen.
+            textarea.style.cssText = 'position: absolute; left: -99999em';
 
-            const newCopyRange = document.createRange();
-            newCopyRange.selectNode(input);
-            selection.addRange(newCopyRange);
+            // Set to readonly to prevent mobile devices opening a keyboard when
+            // text is .select()'ed.
+            textarea.setAttribute('readonly', 'true');
 
-            // const result = document.execCommand('copy');
-            // if (result === false) {
-            //     console.error('Failed to copy text with document.execCommand');
-            // }
+            document.body.appendChild(textarea);
+            textarea.value = newText;
+
+            // Check if there is any content selected previously.
+            const selected = document.getSelection().rangeCount > 0 ?
+                document.getSelection().getRangeAt(0) : false;
+
+            // iOS Safari blocks programmatic execCommand copying normally, without this hack.
+            // https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios
+            if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+                const editable = textarea.contentEditable;
+                textarea.contentEditable = 'true';
+                const range = document.createRange();
+                range.selectNodeContents(textarea);
+                const sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+                textarea.setSelectionRange(0, 999999);
+                textarea.contentEditable = editable;
+            } else {
+                textarea.select();
+            }
 
             try {
                 const successful = document.execCommand('copy');
+
+                // Restore previous selection.
+                if (selected) {
+                    document.getSelection().removeAllRanges();
+                    document.getSelection().addRange(selected);
+                }
+
                 if (successful) {
                     this.updateInternalClipboardText(newText);
                 } else {
@@ -78,7 +98,7 @@ export class ClipboardWrapper {
             } catch (err) {
                 console.log('execCommand error', err);
             } finally {
-                document.body.removeChild(input);
+                document.body.removeChild(textarea);
             }
 
         }
